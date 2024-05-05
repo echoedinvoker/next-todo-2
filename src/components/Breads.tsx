8ab2b6a3231868cc67519ef19aac4effd23cb824
Matt Chang <echoedinvoker@gmail.com>
2024-05-05 17:14:52 +0800

    drop event to get correct ids of breadcrumb item

--- src/components/Breads.tsx
+++ src/components/Breads.tsx
@@ -9,11 +9,40 @@ interface BreadsProps {
 
 export default function Breads({ userId, ids }: BreadsProps) {
   return (
-    <Breadcrumbs variant="bordered" maxItems={5} itemsBeforeCollapse={2} itemsAfterCollapse={2}>
-      <BreadcrumbItem key="Root" href={`/user/${userId}/todos`}>Root</BreadcrumbItem>
+    <Breadcrumbs variant="bordered">
+      <BreadcrumbItem
+        key="Root"
+        href={`/user/${userId}/todos`}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={async (e) => {
+          e.preventDefault();
+          if (ids.length === 0) return;
+          console.log("dropped-todo-id", null);
+          console.log(
+            "dragged-todo-id",
+            e.dataTransfer.getData("dragged-todo-id"),
+          );
+        }}
+      >
+        Root
+      </BreadcrumbItem>
       {ids.map((v, i, a) => (
         <BreadcrumbItem
           key={v.id}
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDrop={async (e) => {
+            e.preventDefault();
+            if (v.id === a.at(-1)!.id) return;
+            console.log("dropped-todo-id", v.id);
+            console.log(
+              "dragged-todo-id",
+              e.dataTransfer.getData("dragged-todo-id"),
+            );
+          }}
           href={`/user/${userId}/todos/${a
             .map((v) => v.id)
             .slice(0, i + 1)
