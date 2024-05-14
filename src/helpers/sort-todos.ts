0a403de1bf5d0c8a3102c23dd25fc52f0b8f39d6
Matt Chang <echoedinvoker@gmail.com>
2024-05-14 21:20:45 +0800

    fix switch reset elasped issue

--- src/helpers/sort-todos.ts
+++ src/helpers/sort-todos.ts
@@ -9,9 +9,16 @@ export async function sortTodos(todos: TodoWithChildren[]) {
   });
   // reset value of order from 1
   for (let i = 0; i < todos.length; i++) {
+    const todo = await db.todo.findUnique({
+      where: { id: todos[i].id },
+      select: { id: true, order: true, updatedAt: true },
+    });
+    if (!todo) {
+      throw new Error("Todos not found");
+    }
     const sortedTodo = await db.todo.update({
       where: { id: todos[i].id },
-      data: { order: i + 1 },
+      data: { order: i + 1, updatedAt: todo.updatedAt },
       include: { children: true },
     });
     sortedTodos.push(sortedTodo);
